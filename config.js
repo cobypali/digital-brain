10d73f36e4b238ee485755a586ec01b4df2dde27
Coby Palivathukal <45637624+cobypali@users.noreply.github.com>
2026-03-08 13:49:40 -0700

    Update Apps Script deployment URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.DIGITAL_BRAIN_CONFIG = {
     // Replace with your deployed Google Apps Script Web App URL.
-    appsScriptUrl: "https://script.google.com/macros/s/AKfycbw1v6G8r2PYLTosSwmrM99MMGnk_1aTGaIy_zYgE2GQrMt6X50Okc6st3nEfJKuWpHI/exec"
+    appsScriptUrl: "https://script.google.com/macros/s/AKfycbwptTsun6rE705eP6CRNCeHDqTDgygTlF4MnhIgwUKGyeLY_Aqmn8HpoffsuUU6xwon/exec"
 };
